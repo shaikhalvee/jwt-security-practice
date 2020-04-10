5cd127e22077f96351d9495e8cb8e1e1f0cc6d8f
Shaikh Islam <shaikhalvee@gmail.com>
2020-04-11 01:50:13 +0600

    1. gradle file update 2. successhandler added 3. config file updated

--- src/main/java/com/practice/jwtsecurity/config/JwtSecurityConfig.java
+++ src/main/java/com/practice/jwtsecurity/config/JwtSecurityConfig.java
@@ -1,14 +1,16 @@
 package com.practice.jwtsecurity.config;
 
+import com.practice.jwtsecurity.security.JwtAuthenticationEntryPoint;
 import com.practice.jwtsecurity.security.JwtAuthenticationProvider;
 import com.practice.jwtsecurity.security.JwtAuthenticationTokenFilter;
+import com.practice.jwtsecurity.security.JwtSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -23,11 +25,17 @@ public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JwtAuthenticationProvider authenticationProvider;
 
+	@Autowired
+	private JwtAuthenticationEntryPoint exceptionEntryPoint;
+
 	// This is the filter.
 	@Bean("jwtAuthTokenFilter")
 	public JwtAuthenticationTokenFilter authenticationTokenFilter() {
-		JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter = new JwtAuthenticationTokenFilter();
-		return jwtAuthenticationTokenFilter;
+		JwtAuthenticationTokenFilter authFilter = new JwtAuthenticationTokenFilter();
+		authFilter.setAuthenticationManager(authenticationManager());
+		// after success some processing
+		authFilter.setAuthenticationSuccessHandler(new JwtSuccessHandler());
+		return authFilter;
 	}
 
 	// This is the authentication Manager that provides auth-provider
@@ -36,5 +44,18 @@ public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new ProviderManager(Collections.singletonList(authenticationProvider));
 	}
 
+	// Http Security Filter configure
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				// csrf token disable
+				.csrf().disable()
+				// any req api having "/rest/" will be authenticated
+				.authorizeRequests().antMatchers("**/rest/**")
+				.authenticated()
+				.and()
+				// If not authenticated, where to redirect
+				.exceptionHandling().authenticationEntryPoint(exceptionEntryPoint)
+	}
 }
 
---
+++ src/main/java/com/practice/jwtsecurity/security/JwtSuccessHandler.java
@@ -0,0 +1,30 @@
+package com.practice.jwtsecurity.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class JwtSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request,
+	                                    HttpServletResponse response,
+	                                    FilterChain chain,
+	                                    Authentication authentication) throws IOException, ServletException {
+		log.debug("Successfully Authentication");
+	}
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request,
+	                                    HttpServletResponse response,
+	                                    Authentication authentication) throws IOException, ServletException {
+		log.debug("Successfully Authentication");
+	}
+}
